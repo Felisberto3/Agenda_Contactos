77f48670416262d485ff91d35663f5aabc61d94d
Felisberto Alberto <felisbertoalberto183@gmail.com>
2025-02-16 05:55:18 -0800

    menu principal criado

---
+++ src/view/MainManu.java
@@ -0,0 +1,14 @@
+package view;
+
+public class MainManu {
+
+    static public void menu() {
+        System.out.println("================= MENU =================");
+        System.out.println("1. Lista Telefonica");
+        System.out.println("2. Adicionar Contactos");
+        System.out.println("3. Actualizar Contactos");
+        System.out.println("4. Deletar Contactos");
+        System.out.println("0. Sair da App");
+    }
+
+}
