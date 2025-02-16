2b62deb60a3c9b94621ca1bb25259885f41d8613
Felisberto Alberto <felisbertoalberto183@gmail.com>
2025-02-16 14:18:36 -0800

    menu

--- src/view/MainManu.java
+++ src/view/MainManu.java
@@ -1,14 +1,18 @@
 package view;
 
+import java.util.Scanner;
+
 public class MainManu {
 
-    static public void menu() {
+    static public int menu(Scanner scan) {
         System.out.println("================= MENU =================");
         System.out.println("1. Lista Telefonica");
         System.out.println("2. Adicionar Contactos");
         System.out.println("3. Actualizar Contactos");
         System.out.println("4. Deletar Contactos");
         System.out.println("0. Sair da App");
+        return scan.nextInt();
+
     }
 
 }
