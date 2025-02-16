e9784e2b1753b1e865539667170d750c79e72a6a
Felisberto Alberto <felisbertoalberto183@gmail.com>
2025-02-16 15:51:23 -0800

    atualiza contactos

--- src/App.java
+++ src/App.java
@@ -1,26 +1,9 @@
 import java.io.IOException;
-import java.util.List;
-
-import com.google.gson.Gson;
 import com.google.gson.JsonIOException;
-
-import model.AgendaModel;
-import model.Contact;
 import view.MainManu;
 
 public class App {
     public static void main(String[] args) throws JsonIOException, IOException {
-        // List<Contact> contact = AgendaModel.allContacts();
-
-        // AgendaModel.save(null);
-
-        // AgendaModel.delete("955371762");
-        // System.out.println(contact);
-
-        // AgendaModel.update("925795149", new Contact("925795149", "Jose Goncalves",
-        // "Canoquena", "ganca@gmail.com"));
-
-        // MainManu.showLista();
         new MainManu().firstMenu();
     }
 }
